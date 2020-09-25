7c1cb144b332b4d3da2362fee6cb952c79fabc0d
MatiasMolina <maty.f.molina4a@gmail.com>
2020-09-25 06:59:37 -0300

    Add:Se visualizan las imagenes, funciona el searchBar y loadMore

--- react-movie/src/components/Home.js
+++ react-movie/src/components/Home.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect }from 'react';
-import {API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
+import React, { useState }from 'react';
+import {POPULAR_BASE_URL, SEARCH_BASE_URL, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 
 
 //imports Components
@@ -14,28 +14,72 @@ import Spinner from './elements/Spinner';
 
 import {useHomeFetch} from './hooks/useHomeFetch';
 
+import NoImage from './images/no_image.jpg';
+
+
 const Home = () => {
-    const [{state,loading,error},fetchMovies] = useHomeFetch();
-    console.log(state);
+    const [
+        {
+            state: {movies, currentPage, totalPages, heroImage},
+            loading,
+            error
+        }
+        ,
+        fetchMovies
+    ] = useHomeFetch();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = search => {
+        const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+        setSearchTerm(search);
+        fetchMovies(endpoint);
+    }
+
+    const loadMoreMovies = () =>{
+        const searchEndPoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage + 1}`;
+        const popularEndPoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
+
+        const endpoint = searchTerm ? searchEndPoint : popularEndPoint;
+
+        fetchMovies(endpoint);
+    }
 
     if(error)return <div>Something went wrong ...</div>;
-    if(!state.movies[0])return <Spinner />;
+    if(!movies[0])return <Spinner />;
 
     return (
     <>
+    { !searchTerm && (
     <HeroImage 
-    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-    title= {state.heroImage.original_title}
-    text= {state.heroImage.overview}
+    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+    title= {heroImage.original_title}
+    text= {heroImage.overview}
     />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <Spinner />
-    <LoadMoreBtn />
+    )}
+    <SearchBar callback={searchMovies} />
+    <Grid header={searchTerm ? 'Search Result':'Popular Movies'}>
+        {movies.map(movie => (
+            <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+                movie.poster_path 
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+            />
+        ))}
+    </Grid>
+    {loading && <Spinner />}
+    {currentPage < totalPages && !loading && (
+        <LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+    )}
     </>
-    )
-}
+    );
+};
 
 
 export default Home;
