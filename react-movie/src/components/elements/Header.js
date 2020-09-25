be0f26ba3ce70cb9338d85748277ae9b09650bd0
MatiasMolina <maty.f.molina4a@gmail.com>
2020-09-24 22:23:49 -0300

    Edit:Se estilizo el "Header" y se crearon style globales

--- react-movie/src/components/elements/Header.js
+++ react-movie/src/components/elements/Header.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
+import RMDBLogo from '../images/reactMovie_logo.png';
+import TMDBLog from '../images/tmdb_logo.svg';
+import {StyledHeader,StyledRMDBLogo,StyledTMDBLogo} from '../styles/StyledHeader';
+//1. Learn how to create a styled basic styled component
+//2. Learn how to handle props in styled component
+//3. Createa global style with styled components
 
-const Header = () => <div>Header</div>
+
+
+
+const Header = () => (
+    <StyledHeader>
+        <div className="header-content">
+            <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo"/>
+            <StyledTMDBLogo src={TMDBLog} alt="tmdb-logo"/>
+        </div>
+    </StyledHeader>
+)
 
 export default Header;
