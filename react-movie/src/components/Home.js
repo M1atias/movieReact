f9401af1bfd05f61df83c536dcccd4b34f6c9c46
MatiasMolina <maty.f.molina4a@gmail.com>
2020-09-24 21:52:21 -0300

    Add:Nuevo proyecto definitivo
    Se crearon componentes, y las configuraciones necesarias para la coneccion con el servicio de peliculas

---
+++ react-movie/src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect }from 'react';
+import {API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
+
+
+//imports Components
+import HeroImage from './elements/HeroImage';
+import SearchBar from './elements/SearchBar';
+import Grid from './elements/Grid';
+import MovieThumb from './elements/MovieThumb';
+import LoadMoreBtn from './elements/LoadMoreBtn';
+import Spinner from './elements/Spinner';
+
+//Custom Hook
+
+import {useHomeFetch} from './hooks/useHomeFetch';
+
+const Home = () => {
+    const [{state,loading,error},fetchMovies] = useHomeFetch();
+    console.log(state);
+    return (
+    <>
+    <HeroImage />
+    <SearchBar />
+    <Grid />
+    <MovieThumb />
+    <Spinner />
+    <LoadMoreBtn />
+    </>
+    )
+}
+
+
+export default Home;
